22f8a850247210159f35518241c303502de8d434
Stefan Wiegmann <mail@stefanwiegmann.de>
2020-04-26 18:03:34 -0400

    readded skeleton bundle

---
+++ lib/stefanwiegmann/skeleton-bundle/StefanwiegmannSkeletonBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Stefanwiegmann\SkeletonBundle;
+
+// use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StefanwiegmannSkeletonBundle extends Bundle
+{
+    // public function build(ContainerBuilder $container)
+    // {
+    //     parent::build($container);
+    // }
+}
